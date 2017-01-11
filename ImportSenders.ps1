94275415c55e9e8b1ee80a958acfcd72eee659c2
Eric <eric.jan@gmx.ch>
2017-01-11 10:43:48 +0100

    Update ImportSenders.ps1
    Removed Whitespace

--- ImportSenders.ps1
+++ ImportSenders.ps1
@@ -1,17 +1,13 @@
 $content = Get-Content C:\Users\user\Desktop\block.txt
 
-
 $CurrentConfig = Get-SenderFilterConfig
 $CurrentBlockedSender = $CurrentConfig.BlockedSenders
 
-
 foreach ($line in $content)
 {
-$CurrentBlockedSender += $line
+	$CurrentBlockedSender += $line
 }
 
-
 Set-SenderFilterConfig -BlockedSenders $CurrentBlockedSender
-
 
 Get-SenderFilterConfig | Format-List BlockedSenders
